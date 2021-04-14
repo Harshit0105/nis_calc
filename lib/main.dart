0bcee1f1059da462c17e7b1ea7c09849c3cf22fd
Harshit0105 <Harshit010501@gmail.com>
2021-04-14 12:53:51 +0530

    Modular Expansion, Modulo, Multiplicative Inverse

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,7 +29,7 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-enum Section { HOME, PAGE_1, PAGE_2 }
+enum Section { HOME, PAGE_1, PAGE_2, PAGE_3 }
 
 class _MyHomePageState extends State<MyHomePage> {
   Section section;
@@ -49,11 +51,15 @@ class _MyHomePageState extends State<MyHomePage> {
         break;
 
       case Section.PAGE_1:
-        body = Page1Section(context);
+        body = PageSection1(); //Multiplicative Inverse
         break;
 
       case Section.PAGE_2:
-        body = Page2Section(context);
+        body = PageSection2(); //Modulcar Exponential
+        break;
+
+      case Section.PAGE_3:
+        body = PageSection3(); //Modulcar Exponential
         break;
     }
 
@@ -77,6 +83,24 @@ class _MyHomePageState extends State<MyHomePage> {
               decoration: BoxDecoration(
                 color: Colors.blue,
               ),
+            ),
+            ListTile(
+              title: Text(
+                'Home',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                setState(() {
+                  section = Section.HOME;
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+            Divider(
+              color: Colors.grey,
             ),
             ListTile(
               title: Text(
@@ -106,10 +130,29 @@ class _MyHomePageState extends State<MyHomePage> {
                     fontSize: 18,
                     fontWeight: FontWeight.bold),
               ),
-              subtitle: Text("Modular Power"),
+              subtitle: Text("Modular Exponential"),
               onTap: () {
                 setState(() {
                   section = Section.PAGE_2;
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+            Divider(
+              color: Colors.grey,
+            ),
+            ListTile(
+              title: Text(
+                'a mod n',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("Modulo"),
+              onTap: () {
+                setState(() {
+                  section = Section.PAGE_3;
                 });
                 Navigator.of(context).pop();
               },
@@ -122,39 +165,205 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-Widget Page2Section(BuildContext context) {
-  return Padding(
-    padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Column(
-          children: [
-            Text(
-              'Welcome',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'a^b mod n',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold),
-            ),
-          ],
-        )
-      ],
-    ),
-  );
+//Modulo
+class PageSection3 extends StatefulWidget {
+  PageSection3({Key key}) : super(key: key);
+
+  @override
+  _PageSection3State createState() => _PageSection3State();
 }
 
-Widget Page1Section(BuildContext context) {
+class _PageSection3State extends State<PageSection3> {
+  TextEditingController aC = new TextEditingController();
+  TextEditingController nC = new TextEditingController();
+  String a, n;
+  String result = '';
+  String doModulo(int a, int n) {
+    var temp1 = a ~/ n;
+    var temp2 = a - temp1 * n;
+    var temp3 = a % n;
+    return "Quo = $temp1 \nRem= $temp2 \na % n = $temp3";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "A mod N",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Text("A: "),
+            TextField(
+              controller: aC,
+              keyboardType: TextInputType.number,
+              onChanged: (value) => a = aC.text.trim(),
+              onSubmitted: (value) => a = aC.text.trim(),
+            ),
+            Divider(),
+            Text("N: "),
+            TextField(
+              controller: nC,
+              keyboardType: TextInputType.number,
+              onChanged: (value) => a = nC.text.trim(),
+              onSubmitted: (value) => a = nC.text.trim(),
+            ),
+            Divider(),
+            // ignore: deprecated_member_use
+            RaisedButton(
+              color: Colors.blue,
+              child: Text("Calculate"),
+              onPressed: () {
+                var temp1 = doModulo(int.parse(aC.text.toString()),
+                    int.parse(nC.text.toString()));
+                print(temp1);
+                setState(() {
+                  result = temp1;
+                });
+              },
+            ),
+            Divider(),
+            Text(
+              result,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//Moduler Exponential
+class PageSection2 extends StatefulWidget {
+  PageSection2({Key key}) : super(key: key);
+
+  @override
+  _PageSection2State createState() => _PageSection2State();
+}
+
+class _PageSection2State extends State<PageSection2> {
+  TextEditingController aC = new TextEditingController();
+  TextEditingController bC = new TextEditingController();
+  TextEditingController nC = new TextEditingController();
+  String a, b, n;
+  String result = '';
+
+  String doPower(int a, int b, int n) {
+    var y = 1;
+    int x = a;
+    int z = b;
+    var binary = b.toRadixString(2);
+    print(binary);
+    var len = binary.length;
+    for (int i = 0; i < len; i++) {
+      print("$y ^ 2 * $x ^ ${binary[i]}");
+      y = (y * y * pow(x, int.parse(binary[i]))) % n;
+    }
+    print(y);
+    return "$x ^ $z % $n = $y";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "A^B mod N",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Divider(
+              color: Colors.grey,
+              height: 5,
+            ),
+            Text("A: "),
+            TextField(
+              controller: aC,
+              keyboardType: TextInputType.number,
+              onChanged: (value) => a = aC.text.trim(),
+              onSubmitted: (value) => a = aC.text.trim(),
+            ),
+            Divider(),
+            Text("B: "),
+            TextField(
+              controller: bC,
+              keyboardType: TextInputType.number,
+              onChanged: (value) => b = bC.text.trim(),
+              onSubmitted: (value) => b = bC.text.trim(),
+            ),
+            Divider(),
+            Text("N: "),
+            TextField(
+              controller: nC,
+              keyboardType: TextInputType.number,
+              onChanged: (value) => a = nC.text.trim(),
+              onSubmitted: (value) => a = nC.text.trim(),
+            ),
+            Divider(),
+            // ignore: deprecated_member_use
+            RaisedButton(
+              color: Colors.blue,
+              child: Text("Calculate"),
+              onPressed: () {
+                var temp1 = doPower(
+                    int.parse(aC.text.toString()),
+                    int.parse(bC.text.toString()),
+                    int.parse(nC.text.toString()));
+                print(temp1);
+                setState(() {
+                  result = temp1;
+                });
+              },
+            ),
+            Divider(),
+            Text(
+              result,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//Multiplicative Inverse
+class PageSection1 extends StatefulWidget {
+  PageSection1({Key key}) : super(key: key);
+
+  @override
+  PageSection1State createState() => PageSection1State();
+}
+
+class PageSection1State extends State<PageSection1> {
   TextEditingController aC = new TextEditingController();
   TextEditingController nC = new TextEditingController();
   String a;
@@ -162,61 +371,93 @@ Widget Page1Section(BuildContext context) {
   String result = '';
   String checkInverse(int a, int n) {
     var temp = n.gcd(a);
-    // if temp==1:
-
-    // def MultiplicativeInverse(n, a):
-    // r1 = int(n)
-    // t1 = 0
-    // r2 = int(a)
-    // t2 = 1
-    // while(r2 > 0):
-    //     q = r1//r2
-    //     r = r1-q*r2
-    //     r1 = r2
-    //     r2 = r
-    //     t = t1-q*t2
-    //     t1 = t2
-    //     t2 = t
-    // gcd = r1
-    // result = t1
-    // if gcd != 1:
-    //     return False, 0
-    // else:
-    //     result = result % n
-    //     return True, result
+    if (temp == 1) {
+      int r1 = n;
+      int t1 = 0;
+      int r2 = a;
+      int t2 = 1;
+      int q, t, r;
+      while (r2 > 0) {
+        q = r1 ~/ r2;
+        r = r1 - q * r2;
+        r1 = r2;
+        r2 = r;
+        t = t1 - q * t2;
+        t1 = t2;
+        t2 = t;
+      }
+      int inverse = t1 % n;
+      result = "$a ^ -1 mod $n = $inverse";
+    } else {
+      result = "Inverse not possible gcd!=1";
+    }
+    return result;
   }
 
-  return Padding(
-    padding: EdgeInsets.all(20),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text("A: "),
-        TextField(
-          controller: aC,
-          keyboardType: TextInputType.number,
-          onChanged: (value) => a = aC.text.trim(),
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "A^(-1) mod N",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Text("A: "),
+            TextField(
+              controller: aC,
+              keyboardType: TextInputType.number,
+              onChanged: (value) => a = aC.text.trim(),
+              onSubmitted: (value) => a = aC.text.trim(),
+            ),
+            Divider(),
+            Text("N: "),
+            TextField(
+              controller: nC,
+              keyboardType: TextInputType.number,
+              onChanged: (value) => a = nC.text.trim(),
+              onSubmitted: (value) => a = nC.text.trim(),
+            ),
+            Divider(),
+            // ignore: deprecated_member_use
+            RaisedButton(
+              color: Colors.blue,
+              child: Text("Calculate"),
+              onPressed: () {
+                var temp1 = checkInverse(int.parse(aC.text.toString()),
+                    int.parse(nC.text.toString()));
+                print(temp1);
+                setState(() {
+                  result = temp1;
+                });
+              },
+            ),
+            Divider(),
+            Text(
+              result,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
         ),
-        Divider(),
-        Text("N: "),
-        TextField(
-          controller: nC,
-          keyboardType: TextInputType.number,
-          onChanged: (value) => a = nC.text.trim(),
-        ),
-        Divider(),
-        // ignore: deprecated_member_use
-        RaisedButton(
-          color: Colors.blue,
-          child: Text("Calculate"),
-          onPressed: () {},
-        ),
-        Divider(),
-      ],
-    ),
-  );
+      ),
+    );
+  }
 }
 
+//Homepage
 Widget HomeSection(BuildContext context) {
   return Padding(
     padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
